9719192da22931db3183adf548ce462a1b8b26f2
BhargaviPeddakota <bhargavicme22040@gmail.com>
2026-06-05 10:12:24 +0530

    adds changes to github

--- Money.java
+++ Money.java
@@ -1,1 +1,15 @@
-// Money.java
+class  Money {
+   int value;
+  Money(int num){
+  this.value=num;
+  }
+  Money add(Money obj){
+   int val = this.value + obj.value;
+   return new Money(val);
+  }
+
+   Money minus(Money obj){
+   int val = this.value - obj.value;
+   return new Money(val);
+  }
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,25 @@
-// StringUtils
+class StringUtils {
+
+  static boolean containsChar(String word,char letter){
+  for(int i=0;i<word.length();i++){
+    if( word.charAt(i) == letter){
+      return true;
+    }
+    
+  }
+  return false;
+}
+
+ static String reverse(String text){
+  String temp = "";
+  for(int i = text.length()-1;i>=0;i--){
+   temp += text.charAt(i);
+  }
+  return temp;
+ }
+
+ static String replaceAll(String text, char oldChar, char newChar){
+  return text.replace(oldChar,newChar);
+ }
+
+}
